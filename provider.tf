6d18b83158926c0abd7fb490718c41ced7a00b17
Doron126 <doronshamoco@gmail.com>
2024-10-25 02:41:14 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
   user           = "administrator@vsphere.local"
-  password       = "D630@bynet"
-  vsphere_server = "10.160.11.234"
+  password       = "password"
+  vsphere_server = "1.1.1.1"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
